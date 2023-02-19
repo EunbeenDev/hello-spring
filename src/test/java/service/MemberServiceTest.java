8b624543ad72e08b78717a14b5a1b4a168d54419
EunbeenDev <ros4312@naver.com>
2023-02-19 16:30:19 +0900

    add Annotation

--- src/test/java/service/MemberServiceTest.java
+++ src/test/java/service/MemberServiceTest.java
@@ -54,6 +54,6 @@ public class MemberServiceTest {
         memberService.join(member1);
         IllegalStateException e=assertThrows(IllegalStateException.class,
                 ()->memberService.join(member2)); //예외 발생해야 함.
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.")
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 }
